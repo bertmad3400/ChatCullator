9978ca12f93d68758c4393ab66581040db6272cf
bertmad3400 <skrivtilbertram@gmail.com>
2024-06-07 15:59:11 +0000

    Add postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import nested from 'postcss-nested';
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+	plugins: [autoprefixer, nested, tailwindcss]
+};
+
+export default config;
