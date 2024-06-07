3041ceecc8382f3f3fb962bf61b99066d7e2da96
bertmad3400 <skrivtilbertram@gmail.com>
2024-06-07 17:52:45 +0000

    Add og tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,15 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>ChatCullator</title>
+	<meta property="og:title" content="ChatCullator - An AI ML LLM Calculator" />
+	<meta
+		property="og:description"
+		content="Ever wondered what would happen if you hooked up ChatGPT to a calculator? Well, wonder no more..."
+	/>
+	<meta property="og:image" content="https://chat-cullator.vercel.app/og-image.png" />
+	<meta property="og:type" content="website" />
+</svelte:head>
+
 <slot />
